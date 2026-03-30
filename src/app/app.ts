fe1c5260bd7bfed34c00f0ff73eb0fc77a202c02
Reza <behruz.mreza@gmail.com>
2026-03-30 15:35:36 +0200

    hero Bereich added.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header/header';
+import { Hero } from './shared/components/hero/hero';
 
 
 @Component({
   selector: 'app-root', 
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
