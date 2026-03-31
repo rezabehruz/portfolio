62428c799b8aaabbca80bb80958d43be562f0abe
Reza <behruz.mreza@gmail.com>
2026-03-31 16:04:04 +0200

    about-me section added.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Hero } from './shared/components/hero/hero';
+import { AboutMe } from "./shared/components/about-me/about-me";
 
 
 
 
 @Component({
   selector: 'app-root', 
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
