7008fbd47b5a4aa48e60811ca55d00fc26a5ced1
Reza <behruz.mreza@gmail.com>
2026-04-01 22:11:41 +0200

    Projects, Testemonials added und Component's SCSS Classes changed

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,14 @@ import { Header } from './shared/components/header/header';
 import { Hero } from './shared/components/hero/hero';
 import { AboutMe } from "./shared/components/about-me/about-me";
 import { Skills } from './shared/components/skills/skills';
+import { Porfolio } from './shared/components/porfolio/porfolio';
 
 
 
 
 @Component({
   selector: 'app-root', 
-  imports: [RouterOutlet, Header, Hero, AboutMe, Skills],
+  imports: [RouterOutlet, Header, Hero, AboutMe, Skills, Porfolio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/shared/components/porfolio/porfolio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-porfolio',
+  imports: [],
+  templateUrl: './porfolio.html',
+  styleUrl: './porfolio.scss',
+})
+export class Porfolio {}
