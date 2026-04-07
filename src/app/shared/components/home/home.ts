1e3ddcac2a072ca6b67a57a6b055d09165412147
Reza <behruz.mreza@gmail.com>
2026-04-07 11:41:59 +0200

    feat: Contact formular.

--- src/app/shared/components/home/home.ts
+++ src/app/shared/components/home/home.ts
@@ -4,10 +4,11 @@ import { Hero } from '../hero/hero';
 import { AboutMe } from '../about-me/about-me';
 import { Skills } from '../skills/skills';
 import { Portfolio } from '../portfolio/portfolio';
+import { ContactMe } from '../contact-me/contact-me';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero, AboutMe, Skills, Portfolio],
+  imports: [Header, Hero, AboutMe, Skills, Portfolio, ContactMe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
