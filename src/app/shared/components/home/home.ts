00df929356641b526e4929860113070bda6a54d9
Reza <behruz.mreza@gmail.com>
2026-04-08 22:28:12 +0200

    footer added.

--- src/app/shared/components/home/home.ts
+++ src/app/shared/components/home/home.ts
@@ -5,10 +5,11 @@ import { AboutMe } from '../about-me/about-me';
 import { Skills } from '../skills/skills';
 import { Portfolio } from '../portfolio/portfolio';
 import { ContactMe } from '../contact-me/contact-me';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero, AboutMe, Skills, Portfolio, ContactMe],
+  imports: [Header, Hero, AboutMe, Skills, Portfolio, ContactMe, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
