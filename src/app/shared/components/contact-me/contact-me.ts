51aa0db17e98522aaf1824bf003eada14d0c47d7
Reza <behruz.mreza@gmail.com>
2026-04-08 20:06:17 +0200

    contact form validation

--- src/app/shared/components/contact-me/contact-me.ts
+++ src/app/shared/components/contact-me/contact-me.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss',
 })
-export class ContactMe {}
+export class ContactMe {
+  contactForm = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required] }),
+    email: new FormControl('', { validators: [Validators.email, Validators.required] }),
+    message: new FormControl('', { validators: [Validators.required, Validators.minLength(20)] }),
+    privacy: new FormControl('', { validators: Validators.requiredTrue }),
+  });
+
+  submitForm() {
+    console.log(this.contactForm);
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  get privacy() {
+    return this.contactForm.get('privacy');
+  }
+}
