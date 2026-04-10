68811321c6b519c7f8ef9d5a6f5ad47c80bf3ff7
Reza <behruz.mreza@gmail.com>
2026-04-10 15:31:22 +0200

    content in Imprint added

--- src/app/shared/components/imprint/imprint.ts
+++ src/app/shared/components/imprint/imprint.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-imprint',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss',
 })
